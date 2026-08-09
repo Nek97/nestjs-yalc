7a27132aef8a9250e58d699497a290d4fa08fc14
AnielloTortora_VitaDev <aniello.tortora@vitameals.com>
2026-08-09 15:00:41 +0200

    refactor(database): enforce strict types for connection helpers

--- database/src/conn.helper.ts
+++ database/src/conn.helper.ts
@@ -1,14 +1,14 @@
 export const DBCONF_PREFIX = 'dbConf_';
 export const CONN_SUFFIX = 'Connection';
 
-export function getConfNameByConnection(connName: string) {
+export function getConfNameByConnection(connName: string): string {
   return `${DBCONF_PREFIX}${connName}`;
 }
 
-export function getConnectionName(dbName: string) {
+export function getConnectionName(dbName: string): string {
   return `${dbName}${CONN_SUFFIX}`;
 }
 
-export function getDBNameByConnection(connName: string) {
+export function getDBNameByConnection(connName: string): string {
   return connName.substring(0, connName.indexOf(CONN_SUFFIX));
 }
